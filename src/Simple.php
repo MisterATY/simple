935db9688e9333af5059d80de51bc7df4f9173c9
Mister ATY <misteraty25@gmail.com>
2023-10-04 12:27:33 +0500

    initial commit

---
+++ src/Simple.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Aty\Simple;
+
+class Simple {
+    public function show() {
+        return "dummy text here...";
+    }
+}
---
+++ src/routes/web.php
@@ -0,0 +1,7 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/testhere', function () {
+	return 'whoa text here';
+});
